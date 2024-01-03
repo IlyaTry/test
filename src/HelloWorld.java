65ddcc50d7dfa54c664d87cb2b81c8e04da1b3c2
Illia Dudnyk <illia.dudnyk@raiffeisen.ua>
2024-01-03 10:31:13 +0200

    Implement random number

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,8 +4,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-       
         System.out.println("Enter number");
         int number = scanner.nextInt();
+
+        int randomNumber = (int) (Math.random() * 101);
+
+
+
     }
 }
